e03a0bf0153d59cac6d7264cb6421ffe17b1fe53
David Fernández Flores <56683644+DavidFFDAW@users.noreply.github.com>
2022-03-03 13:28:27 +0100

    feat(components): new Alert() component added

---
+++ frontend/src/components/alert/index.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from 'react';
+
+export default function Alert({ cssClass, message, seconds }) {
+
+    const alert = useRef('alert');
+    console.log(alert.current);
+
+    useEffect(() => {
+        setTimeout(_ => {
+            alert.current.classList.add('alert-disappear');
+            // alert.current.onAnimationEnd = _ => {
+                alert.current.remove();
+            // }
+        }, seconds );
+    },[]);
+
+    return(
+        <div className="flex center" ref={ alert }>
+            <div className={ `alert ${ cssClass }` }>
+                { message }
+            </div>
+        </div>
+    );
+}
