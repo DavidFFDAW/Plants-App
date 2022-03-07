dcd503f7440999c08b4f771a805d5b472052ea20
David Fernández Flores <56683644+DavidFFDAW@users.noreply.github.com>
2022-03-07 10:07:17 +0100

    refactor(front): updating the imagesURL

--- frontend/src/constants/config.js
+++ frontend/src/constants/config.js
@@ -1,2 +1,2 @@
 export const apiURL = 'http://vps-f87b433e.vps.ovh.net/plants/api/';
-export const imagesURL = 'http://vps-f87b433e.vps.ovh.net/plants/api/';
+export const imagesURL = 'http://vps-f87b433e.vps.ovh.net/plants/images/';
