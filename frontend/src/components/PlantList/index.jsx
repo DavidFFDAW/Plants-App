c9b558a9c2ed6d774d64a8e6d9a5d5931e996b95
davidfernandez@madeinmedia.es <davidfernandez@madeinmedia.es>
2022-03-07 11:55:31 +0100

    css(front): added waterDrop

--- frontend/src/components/PlantList/index.jsx
+++ frontend/src/components/PlantList/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { apiURL } from "../../constants/config";
+import { GiDrop } from "react-icons/gi";
 
 export default function PlantList({ plants, placeholdImg, waterPlant }) {
 
@@ -9,8 +9,9 @@ export default function PlantList({ plants, placeholdImg, waterPlant }) {
                 { plants.map((plant) => (
                     <div className="box-no-padding-total plant-card" key={plant.id} style={{ padding: '30px 0 !important' }}>
                             <div className="plant-card-image">
-                                <img src={plant.image ? plant.image : placeholdImg } alt=""/>
+                                <img src={plant.image ? plant.image : placeholdImg} alt="" />
                             </div>
+                            <GiDrop className="water-drop" />
                             <div className="plant-card-info" style={{ padding: '5px 20px' }}>
                                 <h4 className="plant-card-info-item" style={{ marginBottom:0, fontSize: '20px', textTransform: 'uppercase' }}>{plant.name}</h4>
                                 <p className="plant-card-info-item" style={{ fontSize: '16px', margin: 0 }}>{plant.real_name}</p>
@@ -18,7 +19,7 @@ export default function PlantList({ plants, placeholdImg, waterPlant }) {
                                 <p className="plant-card-info-item" style={{ fontSize: '16px', margin: 0 }}>{plant.last_time_watered}</p>
                             
                                 <div className="down flex between">
-                                        <button className="btn btn-secondary" onClick={ _ => waterPlant(plant.id) }>Regar</button>                              
+                                        <button className="btn btn-secondary" onClick={ _ => waterPlant(plant.id) }><GiDrop/> Regar</button>                              
                                         <Link to={ `/plant/details/${ plant.id }` } className="btn btn-principal">Detalles</Link>                              
                                 </div>
                             </div>
