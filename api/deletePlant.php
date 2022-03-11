bc5d0928e3f8981f14c765207c1bcc126a0f9e00
David Fernández Flores <56683644+DavidFFDAW@users.noreply.github.com>
2022-03-11 10:32:51 +0100

    feat(api): deletion of plant now deletes its image from server

--- api/deletePlant.php
+++ api/deletePlant.php
@@ -1,10 +1,9 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: DELETE');
-
 require_once './functions.php';
 require_once './Plant.php';
+
+headersWithMethod('DELETE');
 
 if (!isset($_GET['id']) || empty($_GET['id']) || !is_numeric((int) $_GET['id'])) {
     json(400,'La petición es incorrecta',true);      
@@ -15,7 +14,15 @@ $plant = Plant::find((int) $_GET['id'], true);
 
 if (!isset($plant) || !$plant) json(404,'Plant not found in DB',true);
 
+$imagesDirPath = dirname(__DIR__).DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR;
+$imageURL = $plant->getImage();
+$imageFileName = str_replace($imageURL,'http://vps-f87b433e.vps.ovh.net/plants/images/','');
+
 $hasBeenRemoved = $plant->remove();
+
+if (file_exists($imagesDirPath.$imageFileName)) {
+    unlink($imagesDirPath.$imageFileName)
+};
 
 if (!$hasBeenRemoved) {
     json(500,'Hubo un problema intentando borrar esta planta');
