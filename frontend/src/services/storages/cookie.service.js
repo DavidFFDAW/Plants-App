78aa1e3b001324a2f87c128077de7b94d665533e
David Fernández Flores <56683644+DavidFFDAW@users.noreply.github.com>
2022-03-15 09:46:16 +0100

    hotfix(front): fixing cookie exports

--- frontend/src/services/storages/cookie.service.js
+++ frontend/src/services/storages/cookie.service.js
@@ -5,19 +5,19 @@ const getParsedCookies = _ => {
     });
 }
 
-export const save = (key,item) => {
+const save = (key,value) => {
     const d = new Date(); // today's date
     d.setDate(d.getDate() + 3); // will expire in three days
     const expires = d.toUTCString();
     document.cookie = `${key}=${value}; expires=${expires}; path=/;`
 }
 
-export const get = (key) => {
+const get = (key) => {
     const cookies = getParsedCookies();
     return cookies[key] || null;
 }
 
-export const remove = (key) => {
+const remove = (key) => {
     document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
 }
 
