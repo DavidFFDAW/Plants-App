22d372633787129f46805c9d8ea9035ce4cb81eb
David Fernández Flores <56683644+DavidFFDAW@users.noreply.github.com>
2022-03-24 12:36:05 +0100

    fixes

--- frontend/src/components/Pagination/Pages.jsx
+++ frontend/src/components/Pagination/Pages.jsx
@@ -3,8 +3,15 @@ export function Pages ({ array, changePage, page }) {
     return (
         <>
             { array.map((item, index) => {
+                const realItem = +item;
+                console.log(realItem);
+                console.log(page);
+
+                const btnType = (+page === realItem) ? 'btn active': 'btn';
+                console.log(btnType);
+
                 return (
-                    <button key={ index } className={ (page === (+item + 1)) ? 'btn active': 'btn' } onClick={ _ => changePage(+item + 1) }>{ item + 1 }</button>
+                    <button key={ index } className={ btnType } onClick={ _ => changePage(+item + 1) }>{ item + 1 }</button>
                 )
             }) }
         </>
