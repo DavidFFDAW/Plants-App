53e0ba5f4cfa8c3cc351a10c49881b728fa8adbd
David Fernández Flores <56683644+DavidFFDAW@users.noreply.github.com>
2022-03-24 12:55:56 +0100

    feat(Pages): styles

--- frontend/src/components/Pagination/Pages.jsx
+++ frontend/src/components/Pagination/Pages.jsx
@@ -5,7 +5,6 @@ export function Pages ({ array, changePage, current }) {
             { array.map((item, index) => {
                 const realItem = +item+1;
                 const btnType = (+current === realItem) ? 'btn active': 'btn';
-                console.log(btnType);
 
                 return (
                     <button key={ index } className={ btnType } onClick={ _ => changePage(realItem) }>{ realItem }</button>
