0cb2242ff5c05d88a05ccedaaf94919a4abe693e
David Fernández Flores <56683644+DavidFFDAW@users.noreply.github.com>
2022-03-24 12:46:08 +0100

    feat(Pages): adding current page to be marked

--- frontend/src/components/Pagination/Pages.jsx
+++ frontend/src/components/Pagination/Pages.jsx
@@ -3,15 +3,12 @@ export function Pages ({ array, changePage, current }) {
     return (
         <>
             { array.map((item, index) => {
-                const realItem = +item;
-                console.log(realItem);
-                console.log(current);
-
+                const realItem = +item+1;
                 const btnType = (+current === realItem) ? 'btn active': 'btn';
                 console.log(btnType);
 
                 return (
-                    <button key={ index } className={ btnType } onClick={ _ => changePage(+item + 1) }>{ item + 1 }</button>
+                    <button key={ index } className={ btnType } onClick={ _ => changePage(realItem) }>{ realItem }</button>
                 )
             }) }
         </>
